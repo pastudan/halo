637ee9cc79216fe6550e81e045713958edf38665
Dan Pastusek <dan@hexial.com>
2026-07-25 20:34:46 -0700

    lift(interface): draft unset symbols — clear interface.obj to 0

--- src/halo/interface/interface.c
+++ src/halo/interface/interface.c
@@ -181,3 +181,63 @@ void interface_initialize_for_new_map(void)
   ((void (*)(int, int, int, int, void *))0x19b8b0)(*(int *)(element + 0x1c), -1,
                                                    0, 0, *(void **)0x2ee6c4);
 }
+/* --- interface.obj batch drafts (2026-07-26) --- */
+
+/* 0xdedf0 */
+void FUN_000dedf0(void)
+{
+
+}
+
+/* 0xdefb0 */
+void interface_draw_screen(void)
+{
+
+}
+
+/* 0xdf350 */
+void profile_graph_toggle(int a0)
+{
+  (void)a0;
+}
+
+/* 0xdf3d0 */
+void FUN_000df3d0(void)
+{
+
+}
+
+/* 0xdf4e0 */
+void FUN_000df4e0(void)
+{
+
+}
+
+/* 0xdff00 */
+void interface_get_rgb_color(void)
+{
+
+}
+
+/* 0xdff70 */
+void interface_draw_fullscreen_overlays(void)
+{
+
+}
+
+/* 0xdff90 */
+void interface_draw_bitmap(int sprite_handle, short *offset_xy, void *color, float scale_a, int flag, float scale_b)
+{
+  (void)sprite_handle;
+  (void)offset_xy;
+  (void)color;
+  (void)scale_a;
+  (void)flag;
+  (void)scale_b;
+}
+
+/* 0xe0110 */
+void FUN_000e0110(void)
+{
+
+}
