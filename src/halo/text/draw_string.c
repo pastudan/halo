0ddeb97c5044dce62ba8ff0ef42d2322452bb40d
Dan Pastusek <dan@hexial.com>
2026-07-26 14:59:54 -0700

    lift(track-a): draw_string get_color/get_string Unicorn-prove (ported:true).

--- src/halo/text/draw_string.c
+++ src/halo/text/draw_string.c
@@ -621,50 +621,16 @@ char FUN_00130b70(void *client __attribute__((unused)), const char *data __attri
 #endif
 
 
-/* draw_string_get_string (0x19b510) — XBE naked draft (batch 278). */
-#if defined(__clang__)
-static void (*const b19b510_assert)(const char *, const char *, int, bool) = display_assert;
-static void (*const b19b510_exitfn)(int) = system_exit;
-static char * (*const b19b510_c19d3c0)(int index, short param_2) = FUN_0019d3c0;
-
-__attribute__((naked, noinline))
-void draw_string_get_string(void)
+/* draw_string_get_string (0x19b510) — readable C lift. */
+char *draw_string_get_string(short index)
 {
-  __asm__ volatile(
-      "pushl %%ebp\n\t"
-      "movl %%esp, %%ebp\n\t"
-      "pushl %%esi\n\t"
-      "movl 0x8(%%ebp), %%esi\n\t"
-      "testw %%si, %%si\n\t"
-      "jl .Ldraw_string_get_string_1\n\t"
-      "cmpw $1, %%si\n\t"
-      "jl .Ldraw_string_get_string_2\n\t"
-      ".Ldraw_string_get_string_1:\n\t"
-      "pushl $1\n\t"
-      "pushl $0x155\n\t"
-      "pushl $0x2b4210\n\t"
-      "pushl $0x2b41e4\n\t"
-      "call *%[assert]\n\t"
-      "pushl $-1\n\t"
-      "call *%[exitfn]\n\t"
-      "addl $0x14, %%esp\n\t"
-      ".Ldraw_string_get_string_2:\n\t"
-      "movl 0x4d9b08, %%eax\n\t"
-      "addl $7, %%esi\n\t"
-      "pushl %%esi\n\t"
-      "pushl %%eax\n\t"
-      "call *%[c19d3c0]\n\t"
-      "addl $8, %%esp\n\t"
-      "popl %%esi\n\t"
-      "popl %%ebp\n\t"
-      "ret\n\t"
-      :
-      : [assert] "m"(b19b510_assert), [exitfn] "m"(b19b510_exitfn), [c19d3c0] "m"(b19b510_c19d3c0)
-      : "memory");
+  if (index < 0 || index >= 1) {
+    display_assert((const char *)0x2b41e4, (const char *)0x2b4210, 0x155, 1);
+    system_exit(-1);
+  }
+  return FUN_0019d3c0(*(int *)0x4d9b08, (short)(index + 7));
 }
-#else
-#error "draw_string_get_string: clang naked draft required"
-#endif
+
 
 
 /* draw_string_set_indents (0x19b5d0) — XBE naked draft (batch 273). */
@@ -719,48 +685,19 @@ void draw_string_set_indents(int width __attribute__((unused)), int height __att
 #endif
 
 
-/* draw_string_get_color (0x19b790) — XBE naked draft (batch 279). */
-#if defined(__clang__)
-static void (*const b19b790_assert)(const char *, const char *, int, bool) = display_assert;
-static void (*const b19b790_exitfn)(int) = system_exit;
-
-__attribute__((naked, noinline))
-void draw_string_get_color(void)
+/* draw_string_get_color (0x19b790) — readable C lift. */
+void draw_string_get_color(float *out)
 {
-  __asm__ volatile(
-      "pushl %%ebp\n\t"
-      "movl %%esp, %%ebp\n\t"
-      "pushl %%esi\n\t"
-      "movl 0x8(%%ebp), %%esi\n\t"
-      "testl %%esi, %%esi\n\t"
-      "jne .Ldraw_string_get_color_1\n\t"
-      "pushl $1\n\t"
-      "pushl $0x188\n\t"
-      "pushl $0x2b4210\n\t"
-      "pushl $0x269fd8\n\t"
-      "call *%[assert]\n\t"
-      "pushl $-1\n\t"
-      "call *%[exitfn]\n\t"
-      "addl $0x14, %%esp\n\t"
-      ".Ldraw_string_get_color_1:\n\t"
-      "movl 0x4d9b18, %%eax\n\t"
-      "movl %%eax, (%%esi)\n\t"
-      "movl 0x4d9b1c, %%ecx\n\t"
-      "movl %%ecx, 0x4(%%esi)\n\t"
-      "movl 0x4d9b20, %%edx\n\t"
-      "movl %%edx, 0x8(%%esi)\n\t"
-      "movl 0x4d9b24, %%eax\n\t"
-      "movl %%eax, 0xc(%%esi)\n\t"
-      "popl %%esi\n\t"
-      "popl %%ebp\n\t"
-      "ret\n\t"
-      :
-      : [assert] "m"(b19b790_assert), [exitfn] "m"(b19b790_exitfn)
-      : "memory");
+  if (!out) {
+    display_assert((const char *)0x269fd8, (const char *)0x2b4210, 0x188, 1);
+    system_exit(-1);
+  }
+  out[0] = *(float *)0x4d9b18;
+  out[1] = *(float *)0x4d9b1c;
+  out[2] = *(float *)0x4d9b20;
+  out[3] = *(float *)0x4d9b24;
 }
-#else
-#error "draw_string_get_color: clang naked draft required"
-#endif
+
 
 
 /* FUN_0019B7E0 (0x19b7e0) — readable C lift. */
@@ -1216,7 +1153,7 @@ static void * (*const b19be30_c19bcc0)(int16_t style, int font_index) = FUN_0019
 static void (*const b19be30_assert)(const char *, const char *, int, bool) = display_assert;
 static void (*const b19be30_exitfn)(int) = system_exit;
 static char * (*const b19be30_c19d3c0)(int index, short param_2) = FUN_0019d3c0;
-static void (*const b19be30_c19d380)(void) = FUN_0019d380;
+static char (*const b19be30_c19d380)(short, const char *) = FUN_0019d380;
 
 __attribute__((naked, noinline))
 void parse_string(void)
